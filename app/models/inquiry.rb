a5a22b54a43b231f73be42b705f36a276c96bf8c
TakashiSato <takashisato.235711@gmail.com>
2016-04-06 18:34:37 +0900

    お問い合わせ画面のスタイル調整、検証メッセージ追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,8 @@
 class Inquiry < ActiveRecord::Base
-  validates :name, :presence => {:message => '名前を入力して下さい'}
-  validates :email, :presence => {:message => 'メールアドレスを入力して下さい'}
+  VALID_EMAIL_REGEXP = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
+
+  validates :name, presence: { message: '名前を入力して下さい' }
+  validates :email,
+   presence: { message: 'メールアドレスを入力して下さい' },
+   format: { with: VALID_EMAIL_REGEXP, message: 'メールアドレスが不正です' }
 end
