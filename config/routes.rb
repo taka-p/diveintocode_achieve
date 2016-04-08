101fdde18be75e03c66231581f975f8ab46be65f
TakashiSato <takashisato.235711@gmail.com>
2016-04-08 13:49:34 +0900

    ユーザ一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :blogs
+  get 'users/index'
+
+  get 'users/show'
 
   devise_for :users
-  root 'top#index'
   get 'company/show' => 'company#show'
   get 'inquiry/index' => 'inquiry#index'
   post 'inquiry/index' => 'inquiry#index'
   post 'inquiry/confirm' => 'inquiry#confirm'
   post 'inquiry/thanks' => 'inquiry#thanks'
+  resources :blogs
+  resources :users, only: [:index, :show]
+
+  root 'top#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408034556) do
+ActiveRecord::Schema.define(version: 20160408043228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160408034556) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.text     "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
