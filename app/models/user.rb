4588e17c80c857a58ebfa600531c7492ef1a2465
TakashiSato <takashisato.235711@gmail.com>
2016-04-11 20:48:50 +0900

    comfirmableを削除（omniauthと併用しちゃだめ..?）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :blogs
 
