1c6a6f6851c67139352de17fccf56ce0a261f53a
TakashiSato <takashisato.235711@gmail.com>
2016-04-11 20:08:54 +0900

    ルーティングからpasswordを除外

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations",
-      passwords: "users/passwords",
       omniauth_callbacks: "users/omniauth_callbacks"
   }
 
