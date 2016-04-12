f2aaf51a1570b53d88f7255dacedf66aac0a791b
TakashiSato <takashisato.235711@gmail.com>
2016-04-12 18:24:09 +0900

    fixture追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# require "csv"
+#
+# CSV.foreach('db/user.csv') do |row|
+#   User.create(id:       row[0],
+#               name:     row[1],
+#               email:    row[2],
+#               profile:  row[3],
+#               password: row[4],
+#               created_at: Time.now,
+#               updated_at: Time.now
+#
+#   )
+# end
