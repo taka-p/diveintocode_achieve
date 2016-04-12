85fd295aa9afa40743549f71c182dfb23bef50f8
TakashiSato <takashisato.235711@gmail.com>
2016-04-12 21:36:20 +0900

    action_mailer修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations",
-      confirmations: "users/confirmations",
       passwords: "users/passwords",
       omniauth_callbacks: "users/omniauth_callbacks"
   }
