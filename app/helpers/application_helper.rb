04142a47a7f4db11c8f07d6ea1ab8b5ce4eafed0
TakashiSato <takashisato.235711@gmail.com>
2016-04-12 13:34:05 +0900

    SNSからプロフィール画像を取得するヘルパーを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def profile_img(user, opt)
+    if user.provider == 'facebook'
+      img_url = "https://graph.facebook.com/#{user.uid}/picture?width=320&height=320"
+    elsif user.provider == 'twitter'
+      img_url = "http://furyu.nazo.cc/twicon/#{user.uid}"
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      img_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    end
+    image_tag(img_url, opt)
+  end
 end
