3c47d22feb4fb225bac3f7e256a17daebe7d8b23
TakashiSato <takashisato.235711@gmail.com>
2016-04-15 01:20:31 +0900

    アカウント削除機能追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string
     super
   end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'アカウントを削除しました'
+  end
 end
