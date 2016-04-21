eb1c3c97a9d083e5ad11ca4c57e3d490ed2031ec
TakashiSato <takashisato.235711@gmail.com>
2016-04-21 12:14:35 +0900

    blogs/showにcommentを配置

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,7 +24,7 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @comments = @blog.comments.build
+    @comment = @blog.comments.build
   end
 
   # GET /blogs/new
