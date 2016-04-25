1674df949d1e260e78f5d674a0a1b5457b039f2e
TakashiSato <takashisato.235711@gmail.com>
2016-04-25 13:31:57 +0900

    Relationshipモデルにfollower_id,followered_idの設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ActiveRecord::Base
+  # フォロー・フォロワーの識別を定義
+  # relationshipsテーブルのfollower_idを参照
+  belongs_to :follower, class_name: 'User'
+  # relationshipsテーブルのfollowed_idを参照
+  belongs_to :followed, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421055101) do
+ActiveRecord::Schema.define(version: 20160425041722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,17 @@ ActiveRecord::Schema.define(version: 20160421055101) do
   add_index "questions", ["category_id"], name: "index_questions_on_category_id", using: :btree
   add_index "questions", ["language_id"], name: "index_questions_on_language_id", using: :btree
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
