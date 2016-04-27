01d8a69984ab0265ba9f2864c1be469e42966e4b
TakashiSato <takashisato.235711@gmail.com>
2016-04-27 13:34:14 +0900

    ヘルパーを作成

---
+++ app/helpers/sessions_helpre.rb
@@ -0,0 +1,27 @@
+module SessionHelper
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+end
