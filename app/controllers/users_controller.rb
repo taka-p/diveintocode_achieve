09daaa0670af7c587b899a6e80d633722d1c0bc2
TakashiSato <takashisato.235711@gmail.com>
2016-04-27 20:20:36 +0900

    ページャーのロジックを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users_total = User.all
     @users = User.page(params[:page]).reverse_order
     @users_left = User.page(params[:page]).limit(10).reverse_order
     @users_right = User.page(params[:page]).limit(10).offset(10).reverse_order
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
 
   def following
     @user = User.find(params[:id])
-    @users = @user.followed_users
+    @users = @users_total = @user.followed_users
     @users_left = @user.followed_users.limit(10)
     @users_right = @user.followed_users.limit(10).offset(10)
     render 'users/index'
@@ -26,7 +27,7 @@ class UsersController < ApplicationController
 
   def followers
     @user = User.find(params[:id])
-    @users = @user.followers
+    @users = @users_total = @user.followers
     @users_left = @user.followers.limit(10)
     @users_right = @user.followers.limit(10).offset(10)
     render 'users/index'
