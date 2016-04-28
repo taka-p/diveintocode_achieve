2440623ed959d1d80c5f84dcfd6df252dd4c0fb9
TakashiSato <takashisato.235711@gmail.com>
2016-04-28 22:27:46 +0900

    タスクコメント、グッジョブボタンのビュー実装

--- spec/views/taskline/tasks/goodjob.html.erb_spec.rb
+++ spec/views/taskline/tasks/goodjob.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "tasks/goodjob.html.erb", type: :view do
+RSpec.describe "tasks/_goodjob.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
