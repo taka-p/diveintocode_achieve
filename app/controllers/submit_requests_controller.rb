ea2fc6924ce31bc4620e9052e493ccd24caa73c9
TakashiSato <takashisato.235711@gmail.com>
2016-05-03 22:16:33 +0900

    ワークフローのコントローラを設定

--- app/controllers/submit_requests_controller.rb
+++ app/controllers/submit_requests_controller.rb
@@ -1,34 +1,92 @@
 class SubmitRequestsController < ApplicationController
+  before_action :set_submit_request, only: [:show, :edit, :update, :destroy, :approve, :unapprove]
+
   def index
+    @submit_requests = SubmitRequest.where(user_id: current_user.id).order(updated_at: :desc)
   end
 
   def new
+    ids = current_user.friend.map { |user| user.id }
+    @users = User.where(user_id: ids)
+    @tasks = Task.where.not(id: SubmitRequest.select(:task_id))
+                 .where(user_id: current_user.id, done: false)
+    @submit_request = current_user.submit_requests.build(status: 1)
+    @user = @submit_request.user
   end
 
   def create
+    @submit_request = SubmitRequest.new(submit_request_params)
+
+    respond_to do |format|
+      if @submit_request.save
+        @submit_request.task.update(status: 1)
+        format.html { redirect_to @submit_request, notice: '依頼を作成しました' }
+      else
+        format.html { rendeer :new }
+      end
+    end
   end
 
   def show
   end
 
   def edit
+    ids = current_user.friend.map { |user| user.id }
+    @users = User.where(user_id: ids)
+    @tasks = Task.where.not(id: SubmitRequest.select(:task_id))
+                 .where(user_id: current_user.id, done: false)
   end
 
   def update
+    respond_to do |format|
+      if @submit_request.update(submit_reqest_params)
+        @submit_request.task.update(charge_id: submit_reqest_params[':charge_id'])
+        format.html { redirect_to @submit_request, notice: '依頼を更新しました' }
+      else
+        format.html { rendeer :edit }
+      end
+    end
   end
 
   def destroy
   end
 
   def approve
+    @submit_request.update(status: 2)
+    @submit_request.task.update(status: 2)
+    @submit_requests = SubmitRequest.where(charge_id: current_user.id).reverse_order
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def unapprove
+    @submit_request.update(stauts: 9, charge_id: @submit_request.user_id)
+    @submit_request = SubmitRequest.where(charge_id: current_user.id).reverse_order
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def reject
+    @submit_request.update(status: 8)
+    @submit_request.task.update(status: 8, charge_id: current_user.id)
+    @submit_requests = SubmitRequest.where(user_id: current_user.id).reverse_order
   end
 
   def inbox
+    @submit_requests = SubmitRequest.where(charge_id: current_user.id).order("updated_at DESC")
   end
+
+  private
+
+    def submit_reqest_params
+      params.require(:submit_request).permit(:task_id, :user_id, :charge_id, :status, :message)
+    end
+
+    def set_submit_request
+      @submit_request = SubmitRequest.find(params[:id])
+    end
 end
