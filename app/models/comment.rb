cc4e24369bbbf81546d71446b3b2f4bc0d9e7a25
TakashiSato <takashisato.235711@gmail.com>
2016-05-07 16:06:18 +0900

    お知らせ機能 modelのアソシエーション設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :blog
   belongs_to :user
+  has_many :notifications, dependent: :destroy
+  has_many :conversations, through: :notifications, source: :comment
+
+  accepts_nested_attributes_for :notifications
+
+  delegate :name, to: :user, prefix: true
 end
