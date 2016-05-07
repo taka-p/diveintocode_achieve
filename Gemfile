3594271bf50fdcad3524af94ca95c7e7d1452178
TakashiSato <takashisato.235711@gmail.com>
2016-05-07 15:11:11 +0900

    Pusher導入

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'rmagick'
 
 gem 'kaminari'
 
+gem 'pusher'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,9 @@ Rails.application.configure do
       :authentication => 'login',
       :enable_starttls_auto => true
   }
+
+  require 'pusher'
+  Pusher.app_id = ENV['PUSHER_APP_ID']
+  Pusher.key    = ENV['PUSHER_KEY']
+  Pusher.secret = ENV['PUSHER_SECRET']
 end
