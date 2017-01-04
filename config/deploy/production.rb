3f09951d464a7936fad878b654606bba56f9a7ea
TakashiSato <takashisato.235711@gmail.com>
2017-01-05 00:57:54 +0900

    IPアドレスをHOST名に変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :production
 
-server '150.95.134.240',      # サーバーのIP
+server 'kirezou.ga',          # サーバーのIP
        user: 'rails',         # ログインユーザー名
        roles: %w{app db web}, # サーバーのロール
        #デプロイするサーバーにsshログインする鍵の情報を記述
